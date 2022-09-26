233bb53bab4d594ec8d1554345ff62af8386d3d3
SmithAldumelo <70138702+SmithAldumelo@users.noreply.github.com>
2022-09-25 17:35:09 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function checkLeapYear(leapYear){
+
+    if ( year % 4 == 0){
+        alert (" it is a leap year")
+
+    } else{
+        alert( "it is not a leap year")
+    }
+
+
+}
+let year = prompt("enter year")
+ checkLeapYear(2022)
